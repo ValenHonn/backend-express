78cd433bb281a427374931f0ac7d0a5158cb1faf
Valen <valentinohonnorat2@gmail.com>
2025-02-21 20:44:16 -0300

    capa services agregada

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,62 +1,71 @@
-import methods from '../utils/fsmethods.js'
+import ProductService from "../services/productsService.js";
 
-const getProducts = async (req, res) => { //
-    const productos = await methods.leerProductos(); //aunque leerProductos ya involucra una operacion asincrona dentro de ella, es necesario volver a definir una operacion asincrona aca, ya que si no espeamos a que termine de leer el archivo, vamos a devolver un json vacio
-    res.json(productos);
+const productService = new ProductService()
+
+const getProducts = async (req, res) => { 
+
+    try {
+        const productos = await productService.getProducts()
+        res.json(productos);
+    }
+    catch(error) {
+        res.status(500).json({ error: 'Error al obtener los productos', error });
+    }
+
 }
 
 const getProductById = async (req, res) => {
-    const productos = await methods.leerProductos();
-    const id = parseInt(req.params.id);
-    const index = productos.findIndex(p => p.id === id);
 
-    if (index === -1) {
-        return res.status(404).json({ error: 'Producto no encontrado' });
+    try {
+        const id = parseInt(req.params.id);
+        const producto = await productService.getProductById(id)
+        res.json(producto);
+    }
+    catch(error) {
+        res.status(500).json({ error: 'Error al obtener los productos', error });
     }
 
-    res.json(productos[index]);
 }
 
 const createProduct = async (req, res) => {
     
-    const productos = await methods.leerProductos(); //productos es un array de objetos de javascript
-    const nuevoProducto = req.body;
-    nuevoProducto.id = productos.length > 0 ? productos[productos.length - 1].id + 1 : 1;
-        
-    productos.push(nuevoProducto); //insertamos al arreglo el nuevo producto
-    await methods.escribirProductos(productos); //sobreescribimos el archivo
-        
-    res.status(201).json(nuevoProducto);
+    try{
+        const nuevoProducto = req.body;
+        const nuevoprod = await productService.createProduct(nuevoProducto)
+        res.status(201).json(nuevoProducto);
+    }
+    catch(error) {
+        console.error('Error en createProduct:', error.message, error.stack); 
+        res.status(500).json({ error: 'Error interno al crear el producto' });
+    }
     
 }
 
 const updateProduct = async (req, res) => {
 
-    const productos = await methods.leerProductos();
-    const id = parseInt(req.params.id);
-    const index = productos.findIndex(p => p.id === id);
-
-    if (index === -1) {
-        return res.status(404).json({ error: 'Producto no encontrado' });
+    try{
+        const id = parseInt(req.params.id);
+        const indiceproducto = await productService.updateProduct(id,req.body)
+        res.json(indiceproducto)
+    }
+    catch(error){
+        console.error('Error en updateProduct:', error.message, error.stack); 
+        res.status(500).json({ error: 'Error interno al actualizar el producto' });
     }
 
-    productos[index] = { id, ...req.body }; 
-    await methods.escribirProductos(productos);
-
-    res.json(productos[index]);
 }
 
 const deleteProduct = async (req, res) => {
-    const productos = await methods.leerProductos();
-    const id = parseInt(req.params.id);
-    const productosFiltrados = productos.filter(p => p.id !== id);
 
-    if (productos.length === productosFiltrados.length) {
-        return res.status(404).json({ error: 'Producto no encontrado' });
+    try {
+        const id = parseInt(req.params.id);
+        const idproducto = await productService.deleteProduct(id)
+        res.json({ mensaje: `Producto con ID ${idproducto} eliminado` });
+    }
+    catch(error) {
+        res.status(500).json({ error: 'Error al eliminar el producto', error });
     }
 
-    await methods.escribirProductos(productosFiltrados);
-    res.json({ mensaje: `Producto con ID ${id} eliminado` });
 }
 
 export default { getProducts, createProduct, updateProduct, deleteProduct, getProductById };
---
+++ src/services/productsService.js
@@ -0,0 +1,55 @@
+import methods from '../utils/fsmethods.js'
+
+class ProductService {
+
+    async getProducts() {  
+
+        return await methods.leerProductos(); //aunque leerProductos ya involucra una operacion asincrona dentro de ella, es necesario volver a definir una operacion asincrona aca, ya que si no espeamos a que termine de leer el archivo, vamos a devolver un json vacio
+
+    }
+
+    async getProductById(id) {
+
+        const productos = await methods.leerProductos();
+        return productos.find(p => p.id === id) || null;
+
+    }
+
+    async createProduct(nuevoProducto) {
+
+        const productos = await methods.leerProductos(); //productos es un array de objetos de javascript
+        nuevoProducto.id = productos.length > 0 ? productos[productos.length - 1].id + 1 : 1;
+            
+        productos.push(nuevoProducto); //insertamos al arreglo el nuevo producto
+        await methods.escribirProductos(productos); //sobreescribimos el archivo
+        return nuevoProducto
+
+    }
+
+    async updateProduct(id, reqbody) {
+
+        const productos = await methods.leerProductos();
+        const index = productos.findIndex(p => p.id === id) || null;
+
+        productos[index] = { id, ...reqbody }; 
+        await methods.escribirProductos(productos);
+        return productos[index];
+
+    }
+
+    async deleteProduct(id) {
+
+        const productos = await methods.leerProductos();
+        const productosFiltrados = productos.filter(p => p.id !== id);
+    
+        if (productos.length === productosFiltrados.length) {
+            return null;
+        }
+    
+        await methods.escribirProductos(productosFiltrados);
+        return id;
+    }
+
+}
+
+export default ProductService
